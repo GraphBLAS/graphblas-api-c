9ec3253bcba3abff3e8487f7fd1f4ed7959fac72
aydozz <aydinbuluc@gmail.com>
2017-01-25 20:38:48 -0800

    eliminating one GrB_ewiseMult

--- GABB17/GabbBC3M.c
+++ GABB17/GabbBC3M.c
@@ -22,7 +22,7 @@ GrB_info BC_update(GrB_Vector *delta, GrB_Matrix A, GrB_index *s, GrB_index nsve
     
   GrB_Matrix numsp;                                      // its nonzero structure holds all vertices that have
   GrB_Matrix_new(&numsp, GrB_INT32, n, nsver);           // been discovered and stores shortest path counts so far.
-  GrB_apply(&numsp,GrB_NULL,GrB_NULL,GrB_IDENTITY_INT32,frontier);      // (deep copy) numsp = frontier
+  GrB_apply(&numsp,GrB_NULL,GrB_NULL,GrB_IDENTITY_I32,frontier);      // (deep copy) numsp = frontier
 
   GrB_Monoid Int32Add;                                   // Monoid <int32_t,+,0>
   GrB_Monoid_new(&Int32Add,GrB_INT32,GrB_PLUS_I32,0);
@@ -39,10 +39,10 @@ GrB_info BC_update(GrB_Vector *delta, GrB_Matrix A, GrB_index *s, GrB_index nsve
   int32_t d = 0;                                         // BFS level number
   int32_t nnz = 0;                                       // nnz == 0 when BFS phase is complete
   do {   // ------------------------ BFS phase -----------------------------
-    // sigmas[d](k,s) = shortest path count to node k from starting vertex s at d^th level
-    GrB_Matrix_new(&(sigmas[d]), GrB_INT32, n, nsver);   // Matrix<int32_t> sigma(n,nsver)
+    // sigmas[d](:,s) = d^th level frontier from starting vertex s at 
+    GrB_Matrix_new(&(sigmas[d]), GrB_BOOL, n, nsver);   // Matrix<Bool> sigma(n,nsver)
       
-    GrB_apply(&(sigmas[d]),GrB_NULL,GrB_NULL,GrB_IDENTITY_INT32,frontier);  // (deep copy) sigma[d,:,:] = frontier
+    GrB_apply(&(sigmas[d]),GrB_NULL,GrB_NULL,GrB_IDENTITY_B,frontier);  // sigma[d] = (Boolean) frontier
     GrB_mxm(&frontier,numsp,GrB_NULL,Int32AddMul,A,frontier,desc);          // update frontier
     GrB_eWiseAdd(&numsp,GrB_NULL,GrB_NULL,Int32AddMul,numsp,frontier,GrB_NULL);// accumulate path counts
     GrB_Matrix_nnz(&nnz,frontier);                       // number of nodes in frontier at this level
@@ -58,7 +58,7 @@ GrB_info BC_update(GrB_Vector *delta, GrB_Matrix A, GrB_index *s, GrB_index nsve
 
   GrB_Matrix nspinv;                                     // inverse of the number of shortest paths
   GrB_Matrix_new(&nspinv, GrB_FP32, n, nsver);
-  GrB_apply(&nspinv,GrB_NULL,GrB_NULL,GrB_MINV_FP32,numsp);                 // nspinv = 1./nsp
+  GrB_apply(&nspinv,GrB_NULL,GrB_NULL,GrB_MINV_F32,numsp);                 // nspinv = 1./nsp
     
   GrB_Matrix bcu;                                        // Betweenness Centrality Updates for each starting vertex in s
   GrB_Matrix_new(&bcu, GrB_FP32, n, nsver);
@@ -67,11 +67,8 @@ GrB_info BC_update(GrB_Vector *delta, GrB_Matrix A, GrB_index *s, GrB_index nsve
   GrB_Matrix w; GrB_Matrix_new(&w,GrB_FP32,n, nsver);    // temporary workspace matrix
   for(int i=d-1; i>0; i--)  // ------------------------ BC computation phase ------------------------
   {
-      GrB_eWiseMult(&w,GrB_NULL,GrB_NULL,FP32Mul,sigmas[i],nspinv,GrB_NULL);
-      GrB_eWiseMult(&w,GrB_NULL,GrB_NULL,FP32Mul,w,bcu,GrB_NULL);           // w = sigmas[i] .* (1 ./ nsp) .* bcu
-      // add contributions made by successors of a node
-      GrB_mxm(&w,GrB_NULL,GrB_NULL,FP32AddMul,A,w,GrB_NULL);                // w = A +.* w
-
+      GrB_eWiseMult(&w,sigmas[i],GrB_NULL,FP32Mul,bcu,nspinv,GrB_NULL);     // w = sigmas[i] .* (1 ./ nsp) .* bcu
+      GrB_mxm(&w,GrB_NULL,GrB_NULL,FP32AddMul,A,w,GrB_NULL);          // w = A +.* w (add contributions by successors)
       GrB_eWiseMult(&w,GrB_NULL,GrB_NULL,FP32Mul,w,sigmas[i-1],GrB_NULL);   // w = w .* sigmas[i-1]
       GrB_eWiseMult(&bcu,GrB_NULL,GrB_PLUS_F32,FP32Mul,w,numsp,GrB_NULL);   // bcu += w .* numsp
   }
