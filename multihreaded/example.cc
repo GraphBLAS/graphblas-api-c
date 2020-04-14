3a1d0aee15a712ce705af1af3a18b75d4b06cc84
Jose Moreira <jmoreira@us.ibm.com>
2020-04-14 17:22:47 -0400

    Made example more ellaborate

--- multihreaded/example.cc
+++ multihreaded/example.cc
@@ -11,7 +11,8 @@ int main
     char      **argv
 )
 { 
-    GrB_Matrix A[N], B[N], C[N], D[N], E[N];  // A, B, C, D, E are "arrays" of matrices
+    GrB_Matrix 	A[N], B[N], C[N], D[N], E[N], F[N], G[N];  // A, B, C, D, E are "arrays" of matrices
+    double	S[N][DIM][DIM];				  // Results for checking
 
     // Create all input matrices.
     for (int i=0; i<N; i++) 
@@ -19,6 +20,24 @@ int main
 	GrB_Matrix_new(A[i]);
 	GrB_Matrix_new(B[i]);
 	GrB_Matrix_new(D[i]);
+	GrB_Matrix_new(F[i]);
+	GrB_Matrix_new(G[i]);
+    }
+
+    // Compute the reference results for testing E = A*B*D;
+    for (int I=0; I<N; I++)
+    {
+	for (int i=0; i<DIM; i++) for (int j=0; j<DIM; j++)
+	{
+	    S[I][i][j] = 0;
+	    for (int k=0; k<DIM; k++)
+	    {
+		double R = 0;
+		for (int l=0; l<DIM; l++)
+		    R += (*A[I])(i,l)*(*B[I])(l,k);
+		S[I][i][j] += R*(*D[I])(k,j);
+	    }
+	}
     }
 
 #pragma omp parallel
@@ -35,29 +54,41 @@ int main
 
 #pragma omp for schedule(dynamic)
 	for (int i=0; i<N; i++)
-	    GrB_mxm(E[i], C[i], D[i]);
-
-    }
-
-    for (int I=0; I<N; I++)
-    {
-	for (int i=0; i<DIM; i++) for (int j=0; j<DIM; j++)
 	{
-	    double S = 0;
-	    for (int k=0; k<DIM; k++)
+	    GrB_mxm(A[i], F[i], G[i]);
+	    GrB_mxm(E[i], C[i], D[i]);
+	}
+
+#pragma omp for schedule(dynamic)
+	for (int I=0; I<N; I++)
+	{
+	    for (int i=0; i<DIM; i++) for (int j=0; j<DIM; j++)
 	    {
-		double R = 0;
-		for (int l=0; l<DIM; l++)
-		    R += (*A[I])(i,l)*(*B[I])(l,k);
-		S += R*(*D[I])(k,j);
+		if (S[I][i][j] != (*E[I])(i,j)) 
+		{
+		    printf("FAIL: S = %f, E[%d](%d,%d) = %f\n", S[I][i][j], I, i, j, (*E[I])(i,j));
+		    exit(-1);
+		}
 	    }
-	    if (S != (*E[I])(i,j)) 
+	}
+
+#pragma omp for schedule(dynamic)
+	for (int I=0; I<N; I++)
+	{
+	    for (int i=0; i<DIM; i++) for (int j=0; j<DIM; j++)
 	    {
-		printf("FAIL: S = %f, E[%d](%d,%d) = %f\n", S, I, i, j, (*E[I])(i,j));
-		exit(-1);
+		double S = 0;
+		for (int k=0; k<DIM; k++)
+		    S += (*F[I])(i,k) * (*G[I])(k,j);
+		if (S != (*A[I])(i,j))
+		{
+		    printf("FAIL: S = %f, A[%d](%d,%d) = %f\n", S, I, i, j, (*A[I])(i,j));
+		    exit(-1);
+		}
 	    }
 	}
     }
+
     printf("PASS\n");
 
     return 0;
